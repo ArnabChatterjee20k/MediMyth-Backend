907783cc14a986f8bedf0166643bebb9dc8f7b87
Arnab <arnabchatterjee.ac.2@gmail.com>
2022-08-31 14:15:41 +0530

    Changed route of Account resource to make it more generic

--- system/doctor/__init__.py
+++ system/doctor/__init__.py
@@ -5,4 +5,4 @@ from system.doctor.Account import Account
 doctor = Blueprint("doctor",__name__)
 api = Api(doctor)
 api.add_resource(Registration,"/register") 
-api.add_resource(Account,"/login") 
+api.add_resource(Account,"/") 
