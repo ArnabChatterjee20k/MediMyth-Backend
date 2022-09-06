df844affb72d14a4bfac75c55f11e6ed27624b92
Arnab <arnabchatterjee.ac.2@gmail.com>
2022-09-06 13:46:22 +0530

    Added a response key in config

--- system/Config.py
+++ system/Config.py
@@ -8,3 +8,4 @@ class Config:
     TWILIO_ACCOUNT_SID = os.environ.get("TWILIO_ACCOUNT_SID")
     TWILIO_AUTH_TOKEN = os.environ.get("TWILIO_AUTH_TOKEN")
     TWILIO_SERVICE_ID = os.environ.get("TWILIO_SERVICE_ID")
+    RESPONSE_KEY = "status"
