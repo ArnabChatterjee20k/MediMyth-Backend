d49dd3b4da8e53b38b835a509a6c27f587a90ac3
Arnab <arnabchatterjee.ac.2@gmail.com>
2022-09-10 09:52:35 +0530

    added correct sid for twilio for notify product

--- system/utils/notify.py
+++ system/utils/notify.py
@@ -1,4 +1,5 @@
 
+import os
 from twilio.rest import Client
 from system.Config import Config
 import json
@@ -8,12 +9,12 @@ ACCOUNT_SID = Config.TWILIO_ACCOUNT_SID
 AUTH_TOKEN = Config.TWILIO_AUTH_TOKEN
 
 client = Client(ACCOUNT_SID, AUTH_TOKEN)
-
+notify_sid = os.environ.get("TWILIO_NOTIFY_SID")
 @celery.task
 def send_bulk_sms(numbers:list, body:str,*args,**kwargs):
     bindings = list(map(lambda number: json.dumps({'binding_type': 'sms', 'address': number.get("contact_number")}), numbers))
     print("=====> To Bindings :>", bindings, "<: =====")
-    notification = client.notify.services("MG0e9c714b83d28bf91be07c522e874e10").notifications.create(
+    notification = client.notify.services(notify_sid).notifications.create(
         to_binding=bindings,
         body=body
     )
