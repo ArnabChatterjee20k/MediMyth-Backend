1e5e78b69116c4d92b3f91cd9f58aebfed18a74b
arnabchatterjee <arnabchatterjee.ac.2@gmail.com>
2022-10-15 13:12:33 +0530

    Made accountschema in less code

--- system/doctor/Schemas/AccountSchema.py
+++ system/doctor/Schemas/AccountSchema.py
@@ -6,7 +6,7 @@ class AccountSchema(Schema):
     name = fields.Str(required=True)
     email = fields.Email(required=True)
     phone_no = fields.Str(required=True,validate=validate.Length(equal=10))
-    reff_code = fields.Str(data_key="referal code")
+    reff_code = fields.Str()
     reg_no = fields.Str(required=True)
     address = fields.Str(required=True)
     category = fields.Str(required=True)
