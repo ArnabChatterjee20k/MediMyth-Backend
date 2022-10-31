e0cc5fedcd723a81570b36c1eed4ad2d619fd5ec
arnabchatterjee <arnabchatterjee.ac.2@gmail.com>
2022-10-31 23:55:47 +0530

    made the feature to send the date and id of the appointment in schedule

--- system/schedule/Schemas/ScheduleDoctorSchema.py
+++ system/schedule/Schemas/ScheduleDoctorSchema.py
@@ -2,9 +2,18 @@ from system.Models.Schedule import Schedule
 ## Appointment model needed to be imported here as Schedule Schema is linked to Appointment
 from system.Models.Appointment import Appointment
 from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
+from marshmallow_sqlalchemy.fields import Nested
+
+
+class Appointmentschema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Appointment
+        ordered = True
+
 
 class ScheduleDoctorSchema(SQLAlchemyAutoSchema):
     class Meta:
         model = Schedule
-        include_relationships = True
-        load_instance = True
+        ordered = True
+    # must be same name as the relationship attribute between Schedule and Appointment Table
+    appointment_data = Nested(Appointmentschema,  many=True , only=("id","date"))
