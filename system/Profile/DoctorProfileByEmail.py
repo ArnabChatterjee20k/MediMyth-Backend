7038c7187a4bcef686f209d505d03980dff09e53
arnabchatterjee <arnabchatterjee.ac.2@gmail.com>
2022-11-08 18:44:42 +0530

    updated the doctor profile by email to accept jwt token to ensure security

--- system/Profile/DoctorProfileByEmail.py
+++ system/Profile/DoctorProfileByEmail.py
@@ -2,9 +2,11 @@ from flask_restful import Resource
 from system.Models.ActiveDoctor import ActiveDoctor
 from system.Models.Doctor import Doctor
 from system.Profile.Schemas.Doctor.ProfileByEmailSchema import ProfileByEmailSchema
-
+from system.utils.JWT import token_required
 class DoctorProfileByEmail(Resource):
-    def get(self,email,*args,**kwargs):
+    @token_required
+    def get(self,*args,**kwargs):
+        email = kwargs.get("email")
         doctor = Doctor.query.join(ActiveDoctor).filter(Doctor.email==email)
         doctor.first_or_404()
         profile = ProfileByEmailSchema()
