a6a5a958265a77e5cd0de48a047ec3e3b526592c
arnab <arnabchatterjee.ac.2@gmail.com>
2022-12-08 08:29:13 +0530

    fixed typo in category routes

--- system/category/CategoryCreator.py
+++ system/category/CategoryCreator.py
@@ -1,9 +1,10 @@
+from flask import jsonify , make_response
 from flask_restful import Resource
 from system.Models.Category import Category
-from system.Config import Config
 from system.category.Schema.GetCategory import GetCategory
 class CategoryCreator(Resource):
     def get(self):
-        categories = Category.query.al()
-        category_schema = GetCategory().dump(categories)
-        return category_schema
+        categories = Category.query.all()
+        print(categories)
+        category_schema = GetCategory().dump(categories,many=True)
+        return make_response(jsonify(category_schema),200)
