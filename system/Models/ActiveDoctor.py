57ffcc4517d1bddc32c1f2839776b9e207b4905d
arnab <arnabchatterjee.ac.2@gmail.com>
2022-12-09 11:52:06 +0530

    changed backref name vacation data to active_doctor

--- system/Models/ActiveDoctor.py
+++ system/Models/ActiveDoctor.py
@@ -5,7 +5,7 @@ class ActiveDoctor(db.Model):
     doctor_id = db.Column(db.Integer,db.ForeignKey("doctor.id",onupdate="CASCADE",ondelete="CASCADE"),nullable=False,unique=True)
     active_doctor_id = db.Column(db.String,unique=True)
     scheduled_data = db.relationship("Schedule",backref="scheduled_data",lazy="dynamic",cascade='all,delete',passive_deletes=True)
-    vacation_data = db.relationship("Vacation",backref="vacation_data",lazy="dynamic",cascade='all,delete',passive_deletes=True)
+    vacation_data = db.relationship("Vacation",backref="active_doctor",lazy="dynamic",cascade='all,delete',passive_deletes=True)
     # @hybrid_property
     # def active_doctor_id(self):
     #     return f"MMD-{self.id}"
