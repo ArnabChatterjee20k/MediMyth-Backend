a68ac0e2e15a415a1d51277fecda5e337a69e0f1
arnab <arnabchatterjee.ac.2@gmail.com>
2022-12-11 13:37:52 +0530

    added the vacation checking of doctor in appointment booking

--- system/Models/Appointment.py
+++ system/Models/Appointment.py
@@ -2,50 +2,58 @@ from system import db
 from datetime import datetime
 from sqlalchemy.dialects.postgresql import DATE
 from system.Models.Schedule import Schedule
+from system.Models.Vacation import Vacation
 from system.utils.convert_str_to_date import convert_str_to_date
 from system.utils.datetime_fns import isInAppointmentRange
 
+
 class Appointment(db.Model):
     # this is for patients who will book to a schedule
-    id = db.Column(db.Integer,primary_key = True)
+    id = db.Column(db.Integer, primary_key=True)
     appointment_id = db.Column(db.String)
-    schedule_id = db.Column(db.Integer,db.ForeignKey("schedule.id",onupdate="CASCADE",ondelete="CASCADE"),nullable=False)
-    appointment_id = db.Column(db.String,unique=True)
+    schedule_id = db.Column(db.Integer, db.ForeignKey(
+        "schedule.id", onupdate="CASCADE", ondelete="CASCADE"), nullable=False)
+    appointment_id = db.Column(db.String, unique=True)
     person_id = db.Column(db.Integer)
-    name = db.Column(db.String(30),nullable=False)
-    contact_number = db.Column(db.String(10),nullable=False)##contact number of the patient
-    age = db.Column(db.Integer,nullable=False)
-    appointment_date = db.Column(DATE(),nullable=False) ## this is only for the date in which we will meet the doctor. The booking start time and booking end time will be based on current date and time
-    date = db.Column(db.DateTime,default = datetime.utcnow)
+    name = db.Column(db.String(30), nullable=False)
+    # contact number of the patient
+    contact_number = db.Column(db.String(10), nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    # this is only for the date in which we will meet the doctor. The booking start time and booking end time will be based on current date and time
+    appointment_date = db.Column(DATE(), nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
 
     # @hybrid_property
     # def appointment_id(self):
     #     return f"MMA-{self.id}"
 
-
     @classmethod
-    def check_booking(cls,id,appointment_date):
+    def check_booking(cls, id, appointment_date):
         schedule = Schedule.query.filter_by(id=id).first_or_404()
         params = {
-            "provided_date" : appointment_date,
-            "scheduled_day" : schedule.day, 
-            "starting_day":schedule.booking_start, 
-            "end_hour" : schedule.booking_end,
-            "slot_start":schedule.slot_start,
-            "req_specfic_week" : schedule.specific_week
+            "provided_date": appointment_date,
+            "scheduled_day": schedule.day,
+            "starting_day": schedule.booking_start,
+            "end_hour": schedule.booking_end,
+            "slot_start": schedule.slot_start,
+            "req_specfic_week": schedule.specific_week
         }
-        print(cls.check_limit(schedule,appointment_date))
-        return cls.check_limit(schedule,appointment_date) and isInAppointmentRange(**params)
-    
+        print(cls.check_limit(schedule, appointment_date))
+        return cls.check_limit(schedule, appointment_date) and isInAppointmentRange(**params)
 
-    @classmethod 
-    def check_limit(cls,schedule,appointment_date):
+    @classmethod
+    def check_vacation(cls, id, appointment_date):
+        print(appointment_date)
+        print(Schedule.query.join(Schedule.vacation).filter(Schedule.id == id, Vacation.start <= appointment_date, Vacation.end >= appointment_date).first())
+        return Schedule.query.join(Schedule.vacation).filter(Schedule.id == id, Vacation.start <= appointment_date, Vacation.end >= appointment_date).first()
+
+    @classmethod
+    def check_limit(cls, schedule, appointment_date):
         schedule_id = schedule.id
         appointment_date_obj = convert_str_to_date(appointment_date)
-        appointments = cls.query.filter(cls.schedule_id==schedule_id,cls.appointment_date==appointment_date_obj).count()
+        appointments = cls.query.filter(
+            cls.schedule_id == schedule_id, cls.appointment_date == appointment_date_obj).count()
         limit = schedule.patient_limit
-        if limit and appointments>=limit:
+        if limit and appointments >= limit:
             return False
         return True
-    
-    
--- system/appointment/AppointmentHandler.py
+++ system/appointment/AppointmentHandler.py
@@ -1,6 +1,6 @@
 from flask_restful import Resource
 from system import db
-from flask import make_response , request
+from flask import make_response, request
 from system.Models.Appointment import Appointment
 from system.Models.Schedule import Schedule
 from system.appointment.utils.verifyAppointmentData import verify_appointment
@@ -8,31 +8,40 @@ from system.appointment.Schemas.AppointmentDataSchema import AppointmentDataSche
 from system.utils.otp_required import otp_required
 from system.Config import Config
 import traceback
+
+
 class AppointmentHandler(Resource):
-    def get(self,schedule_id):
+    def get(self, schedule_id):
         # if date is provided as a filter then we will use it to filter all the appointments in that date
         appointment_date = request.args.get("date")
-        filter = {"schedule_id":schedule_id}
+        filter = {"schedule_id": schedule_id}
         if appointment_date:
             filter["appointment_date"] = appointment_date
         schema = AppointmentDataSchema()
         data = Appointment.query.filter_by(**filter).all()
-        ## if schedule not exist then empty array
-        return schema.dump(data,many=True)
+        # if schedule not exist then empty array
+        return schema.dump(data, many=True)
 
     @verify_appointment
     @otp_required
-    def post(self,schedule_id,**data):
+    def post(self, schedule_id, **data):
         data = data.get("update")
-        exist = Appointment.query.filter_by(schedule_id=schedule_id,**data).first()
+        exist = Appointment.query.filter_by(
+            schedule_id=schedule_id, **data).first()
         if exist:
-            return make_response({Config.RESPONSE_KEY:"already registered"},403)    
+            return make_response({Config.RESPONSE_KEY: "already registered"}, 403)
+        
         appointment_date = data.get("appointment_date")
-        if Appointment.check_booking(schedule_id,appointment_date):
-                appointment = Appointment(schedule_id=schedule_id,**data)
-                db.session.add(appointment)
-                db.session.flush()
-                appointment.appointment_id = f"MMA-{appointment.id}"
-                db.session.commit()
-                return make_response({Config.RESPONSE_KEY:"success","appointment_id":appointment.appointment_id})    
-        return make_response({Config.RESPONSE_KEY:"plz check the schedule"},404)
+
+        if Appointment.check_vacation(id=schedule_id, appointment_date=appointment_date):
+            return make_response({Config.RESPONSE_KEY: "Doctor On Vacation"},400)
+
+        if Appointment.check_booking(schedule_id, appointment_date):
+            appointment = Appointment(schedule_id=schedule_id, **data)
+            db.session.add(appointment)
+            db.session.flush()
+            appointment.appointment_id = f"MMA-{appointment.id}"
+            db.session.commit()
+            return make_response({Config.RESPONSE_KEY: "success", "appointment_id": appointment.appointment_id})
+
+        return make_response({Config.RESPONSE_KEY: "plz check the schedule"}, 404)
