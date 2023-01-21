1bb766ce6ed76f14b4c3296830333b32af1601f1
arnab <arnabchatterjee.ac.2@gmail.com>
2023-01-21 16:31:41 +0530

    schema update - made the category table category field unique

--- system/Models/Category.py
+++ system/Models/Category.py
@@ -2,4 +2,4 @@ from system import db
 
 class Category(db.Model):
     id = db.Column(db.Integer,primary_key = True,nullable=False)
-    category = db.Column(db.String,nullable=False)
+    category = db.Column(db.String,nullable=False,unique=True)
