8ebfe0f3ccd36ad0f3c9364154dcce3f3bfd8364
arnab <arnabchatterjee.ac.2@gmail.com>
2023-03-04 10:09:10 +0530

    modified config

--- system/Config.py
+++ system/Config.py
@@ -1,11 +1,12 @@
 import os
 from dotenv import load_dotenv , dotenv_values
-# load_dotenv()
+load_dotenv()
 class Config:
     SECRET_KEY = "761f9d132273fe311f3e13be9faa56b6"
     PRODUCTION = False
     DEBUG = not PRODUCTION
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:arnab@localhost/medimyth"
+    # SQLALCHEMY_DATABASE_URI = "postgresql://postgres:arnab@localhost/medimyth"
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DB_URI")
 
     SQLALCHEMY_TRACK_MODIFICATIONS = True
 
@@ -18,8 +19,8 @@ class Config:
     
     ALGOLIA_APP_ID = os.environ.get("ALGOLIA_APP_ID")
     ALOGOLIA_API_KEY_ADMIN = os.environ.get("ALOGOLIA_API_KEY_ADMIN")
-    # ALGOLIA_INDEX = os.environ.get("ALGOLIA_INDEX")
-    ALGOLIA_INDEX = "medimyth_doctors"
+    ALGOLIA_INDEX = os.environ.get("ALGOLIA_INDEX")
+    # ALGOLIA_INDEX = "medimyth_doctors"
 
     AWS_REGION = os.environ.get("AWS_REGION")
     AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
